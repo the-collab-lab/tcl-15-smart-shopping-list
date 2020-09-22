172e59625f16c41278cd26f122875be4b0582b40
Lauren Beatty <lauren.beatty@treasure-data.com>
2020-09-21 19:44:31 -0500

    add firebase config values for tcl-15

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
+  apiKey: 'AIzaSyDhxP3R-9sojgJEO8MO7RyWx2mnMlfMKfA',
+  authDomain: 'tcl-15-smart-shopping-list.firebaseapp.com',
+  databaseURL: 'https://tcl-15-smart-shopping-list.firebaseio.com',
+  projectId: 'tcl-15-smart-shopping-list',
+  storageBucket: 'tcl-15-smart-shopping-list.appspot.com',
+  messagingSenderId: '1084110216384',
+  appId: '1:1084110216384:web:1282dfa7744f7df182a125',
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
