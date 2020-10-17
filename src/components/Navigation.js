2db37c305b179d9e43fc05ff2f7517cb3e44ade1
Sarah <prosarah10@gmail.com>
2020-10-17 15:20:29 +0200

    Style: add some tweaks to the nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../css/components/main.css';
 import '../css/components/Navigation.css';
 
 const Navigation = ({ hasToken }) => {
