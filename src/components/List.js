cd5d135f5eaf6413ce7485b073f6f427ab66b59a
Sarah Eisa <prosarah10@gmail.com>
2020-10-26 18:21:18 +0200

    Feat: Update item in firestore

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { FirestoreCollection } from 'react-firestore';
+import { firebase } from '../lib/firebase';
 import '../css/components/ItemsList.css';
 import { getToken } from '../lib/TokenService';
+import { shoppingLists, getShoppingList } from '../lib/shoppingListsCollection';
 
 export default function List() {
+  const checkItem = (index, item) => {
+    getShoppingList(getToken())
+      .then((data) => {
+        const items = data.docs[0].data().items;
+        items[index] = {
+          ...item,
+          isChecked: true,
+          lastPurchased: new Date(),
+        };
+
+        shoppingLists().doc(data.docs[0].id).update({
+          items,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>LIST OF ITEMS</h1>
@@ -17,9 +38,12 @@ export default function List() {
             <div>
               {data[0] ? (
                 <ul>
-                  {data[0].items.map((item) => (
+                  {data[0].items.map((item, index) => (
                     <li key={item.name} className="list-item">
-                      <input type="checkbox" />
+                      <input
+                        type="checkbox"
+                        onChange={(e) => checkItem(index, item)}
+                      />
                       <div className="name">{item.name}</div>
                     </li>
                   ))}
