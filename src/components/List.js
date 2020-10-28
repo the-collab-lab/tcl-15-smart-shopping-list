aae01bdee8eabe9cc5d34ced602780654a568937
hajar nasr <hajar-nasr@hotmail.com>
2020-10-28 22:37:13 +0200

    refactor: use FirestoreDocument instead of FirestoreCollection

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FirestoreCollection } from 'react-firestore';
+import { FirestoreDocument } from 'react-firestore';
 import ListItem from './ListItem';
 import '../css/components/ItemsList.css';
 import { getToken } from '../lib/TokenService';
@@ -8,25 +8,30 @@ export default function List() {
   return (
     <div>
       <h1>LIST OF ITEMS</h1>
-      <FirestoreCollection
-        path="shoppingLists"
-        filter={['token', '==', getToken()]}
+      <FirestoreDocument
+        path={`shoppingLists/${getToken()}`}
         render={({ isLoading, data }) => {
+          const itemsKeys = data
+            ? Object.keys(data).filter((key) => key !== 'id')
+            : '';
           return isLoading ? (
             <div className="m-auto">Loading</div>
           ) : (
             <div>
-              {data[0] ? (
+              {itemsKeys.length >= 1 ? (
                 <ul>
-                  {data[0].items.map((item, index) => (
-                    <ListItem
-                      listItem={item}
-                      index={index}
-                      key={index}
-                      listId={data[0].id}
-                      items={data[0].items}
-                    />
-                  ))}
+                  {itemsKeys
+                    .sort((a, b) => (a > b ? 1 : -1))
+                    .map((key) => {
+                      return (
+                        <ListItem
+                          listItem={data[key]}
+                          key={key}
+                          listId={data.id}
+                          itemId={key}
+                        />
+                      );
+                    })}
                 </ul>
               ) : (
                 <div>Your list is empty!!</div>
