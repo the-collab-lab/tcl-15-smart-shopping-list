2a8fd9d2505d552c0fb321476bc8278c93d60742
hajar nasr <hajar-nasr@hotmail.com>
2020-11-03 20:36:14 +0200

    refactor: sort the SortedList in a more efficient way

--- src/components/SortedList.js
+++ src/components/SortedList.js
@@ -5,7 +5,8 @@ const SortedList = ({ itemsKeys, data }) => {
   return (
     <ul>
       {itemsKeys
-        .sort((a, b) => (a > b ? 1 : -1))
+        // a stolen line from Anderw's blog
+        .sort((a, b) => (a > b ? 1 : a < b ? -1 : 0))
         .map((key) => {
           return <ListItem listItem={data[key]} key={key} itemId={key} />;
         })}
