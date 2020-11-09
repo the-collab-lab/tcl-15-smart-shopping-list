fdd8adf6a8f16b6c10ccb0454ef2b8bd04680921
hajar nasr <hajar-nasr@hotmail.com>
2020-11-09 16:57:58 +0200

    feat: sort items by the estimated number of days and alphabetically

--- src/components/SortedList.js
+++ src/components/SortedList.js
@@ -8,7 +8,18 @@ const SortedList = ({ itemsKeys, data }) => {
         // This explicitly compares between a or b, in order to sort the items by the keys
         // If a is greater than b, it will return 1, else if a is less than b it will return -1
         // Or else, it will return 0
-        .sort((a, b) => (a > b ? 1 : a < b ? -1 : 0))
+        .sort((a, b) => {
+          let [howSoonA, nameA] = [data[a].howSoon, data[a].name];
+          let [howSoonB, nameB] = [data[b].howSoon, data[b].name];
+
+          return howSoonA > howSoonB
+            ? 1
+            : howSoonA < howSoonB
+            ? -1
+            : nameA > nameB
+            ? 1
+            : -1;
+        })
         .map((key) => (
           <ListItem listItem={data[key]} key={key} itemId={key} />
         ))}
