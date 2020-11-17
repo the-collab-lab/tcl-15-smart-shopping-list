53955c9211fa74eb33ba83f51a9a873d4999dc65
Sarah Eisa <prosarah10@gmail.com>
2020-11-17 13:19:45 +0200

    Feat: Close the modal by clicking anywhere in the window
    - Added a container for the modal with full width&height
    to close the modal whenever you click on the window

--- src/components/ListItemDetails.js
+++ src/components/ListItemDetails.js
@@ -5,32 +5,35 @@ import '../css/components/modal.css';
 const ListItemDetails = ({ listItem, isShowModal, hideModal }) => {
   const toggleModal = isShowModal ? 'display-block' : 'display-none';
   return (
-    <div className={`modal ${toggleModal}`}>
-      <div className="modal-header">
-        <h1>{listItem.name}</h1>
-        <button onClick={hideModal}>X</button>
-      </div>
-      <div className="modal-body">
-        {listItem.recentPurchase ? (
-          <>
-            <p>Last purchased: {formatDate(listItem['recentPurchase'])}</p>
-            <p>
-              Next purchase:{' '}
-              {formatDate(
-                calculateNextPurchase(
-                  listItem['howSoon'],
-                  listItem['recentPurchase'],
-                ),
-              )}
-            </p>
-            <p>
-              You have purchased this item {listItem['numberOfPurchases']} time
-              {listItem['numberOfPurchases'] > 1 && 's'}.
-            </p>
-          </>
-        ) : (
-          <p>Not purchases yet</p>
-        )}
+    <div className={`modal ${toggleModal}`} onClick={hideModal}>
+      <div className="modal-container">
+        <div className="modal-header">
+          <h1>{listItem.name}</h1>
+          <button onClick={hideModal}>X</button>
+        </div>
+        <div className="modal-body">
+          {listItem.recentPurchase ? (
+            <>
+              <p>Last purchased: {formatDate(listItem['recentPurchase'])}</p>
+              <p>
+                Next purchase:{' '}
+                {formatDate(
+                  calculateNextPurchase(
+                    listItem['howSoon'],
+                    listItem['recentPurchase'],
+                  ),
+                )}
+              </p>
+              <p>
+                You have purchased this item {listItem['numberOfPurchases']}{' '}
+                time
+                {listItem['numberOfPurchases'] > 1 && 's'}.
+              </p>
+            </>
+          ) : (
+            <p>Not purchases yet</p>
+          )}
+        </div>
       </div>
     </div>
   );
