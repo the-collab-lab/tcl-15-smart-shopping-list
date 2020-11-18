3faef95ebb67afd69f0a6d9636b687bb1c298ced
deboragaleano <dgaleanoservian@gmail.com>
2020-11-18 18:55:27 +0100

    style: add bootstrap to the welcome page

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import InputField from './InputField';
+import { Button } from 'react-bootstrap';
 
 const Form = (props) => {
   const [inputValue, setInputValue] = useState('');
@@ -20,7 +21,7 @@ const Form = (props) => {
         }}
       />
       {children}
-      <button className={submitBtn.className}>{submitBtn.text}</button>
+      <Button variant="dark">{submitBtn.text}</Button>
     </form>
   );
 };
